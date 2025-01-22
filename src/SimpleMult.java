96833a8a018d935f7a2648268d4aa9d547a01e2a
EbediMeleck <ituzeebedi12@gmail.com>
2025-01-22 02:30:51 -0800

    some file

--- src/SimpleMult.java
+++ src/SimpleMult.java
@@ -1,6 +1,14 @@
+import javax.swing.*;
+
 public class SimpleMult {
     public static void main(String[] args) {
- System.out.println("Try To solve problem and then write code");
+        JFrame frame = new JFrame("My Frist Java App");
+        frame.setSize(450,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel label = new JLabel("Solve Problem then write code ",SwingConstants.CENTER)
+;
+        frame.add(label);
+        frame.setVisible(true);
     }
 }
 
